11d38c07e0662bfcb727fb1708a43bb7cb5fc59c
Chris York <christopherayork1992@gmail.com>
2019-12-05 00:36:58 -0500

    lat and lon were strings, needed to be floats!

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -34,7 +34,7 @@ def cityreader(cities=[]):
     with open('cities.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            cities.append(City(row['city'], row['lat'], row['lng']))
+            cities.append(City(row['city'], float(row['lat']), float(row['lng'])))
     return cities
 
 
